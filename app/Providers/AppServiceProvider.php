359012bc1bda08f03a54802c77ad732857278627
solomon <sergious-91@mail.ru>
2017-08-13 17:19:32 +0300

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
             'user'           => ( \Auth::check() ) ? \Auth::user() : false,
             'current_uri'    => \Request::path(),
         ]);
+
         Schema::defaultStringLength(191);
     }
 
