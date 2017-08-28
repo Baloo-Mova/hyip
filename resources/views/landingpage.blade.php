1473e716ac40d45f74a1e5681beb62edb38a7129
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-08-28 23:57:55 +0300

    update landing

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -14,7 +14,31 @@
 
 
 <!-- Fixed navbar -->
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-green navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                <img alt="Brand" src="{{ asset('img/logo.png') }}">
+            </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse top-main-menu__wrap">
+            <ul class="nav navbar-nav top-main-menu__menu_left new_nav" id="menu">
+                <li>
+                    @foreach($data['contacts']['social']['links'] as $soc)
+                        <a href="{{ $soc['link'] }}" class="main-menu__social-link"><img src="{{ asset($soc['img'].".svg") }}" alt="" class="main-menu__social-link__img"></a>
+                    @endforeach
+                </li>
+            </ul>
+
+        </div><!--/.nav-collapse -->
+        <ul class="nav navbar-nav navbar-right top-main-menu__menu_right hidden-sm hidden-xs" >
+            <li><a href="#regulations" class="anchor_a">Feedback</a></li>
+            <li><a href="{{ url('/login') }}">Login</a></li>
+            <li><a href="{{ url('/register') }}">Registration</a></li>
+        </ul>
+    </div>
+</nav>
+<nav class="navbar navbar-default navbar-static-top">
     @include('partial.mainmenu')
 </nav>
 
@@ -27,11 +51,11 @@
             <div class="video_container_after"></div>
             <div class="owl-carousel main-carousel">
                 @foreach($data['carousel'] as $carousel)
-                <div class="carousel-item" >
-                    <h3 class="carousel-caption">
-                        {{ $carousel['caption'] }}
-                    </h3>
-                </div>
+                    <div class="carousel-item">
+                        <h3 class="carousel-caption">
+                            {{ $carousel['caption'] }}
+                        </h3>
+                    </div>
                 @endforeach
             </div>
         </div>
@@ -41,12 +65,23 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <div class="greetings_img_wrap">
-                    <img src="{{ $data['greetings']['img'] }}" alt="" class="greetings_img">
+                <div class="greetings__wrap row">
+                    <div class="col-xs-12">
+                        <h1 class="greetings_title">WELCOME TO {{ env("APP_NAME") }}</h1>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="greetings_img_wrap">
+                            <img src="{{ $data['greetings']['img'] }}" alt="" class="greetings_img">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <h4 class="greetings_subtitle">Trust Management</h4>
+                        <p class="greetings_txt">
+                            {{ $data['greetings']['description'] }}
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
                 </div>
-                <p class="greetings_txt">
-                    {{ $data['greetings']['description'] }}
-                </p>
             </div>
         </div>
     </div>
@@ -60,23 +95,81 @@
         </div>
         <div class="row">
             @foreach($data['about'] as $about)
-            <div class="col-xs-12 col-md-6 about__item">
-                <div class="col-xs-4 about-icon__wrap">
-                    <i class="{{ $about['icon'] }} fa-6 about-fa" aria-hidden="true"></i>
-                </div>
-                <div class="col-xs-8">
-                    <h4>{{ $about['title'] }}</h4>
-                    <div class="overflow about__overflow">
-                        <p>{{ $about['description'] }}</p>
+                <div class="col-xs-12 col-md-6 about__item">
+                    <div class="col-xs-4 about-icon__wrap">
+                        <i class="{{ $about['icon'] }} fa-6 about-fa" aria-hidden="true"></i>
                     </div>
-                    <a href="{{ route('about') }}" class="about__link">Подробнее...</a>
+                    <div class="col-xs-8">
+                        <h4>{{ $about['title'] }}</h4>
+                        <div class="overflow about__overflow">
+                            <p>{{ $about['description'] }}</p>
+                        </div>
+                        <a href="{{ route('about') }}" class="about__link">Подробнее...</a>
+                    </div>
                 </div>
-            </div>
             @endforeach
         </div>
         <div class="row">
             <div class="col-xs-12 about__register-wrap">
                 <a href="{{ url('register') }}" class="btn btn-success btn-lg">Регистрация</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="steps" class="steps">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>3 шага, чтобы начать зарабатывать</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 l-steps">
+                <div class="row">
+                    <div class="col-md-4 cut">
+                        <div class="l-steps__item">
+                            <div class="l-steps__item-number">
+                                <span class="kostil_1">1.</span>
+                            </div>
+                            <div class="l-steps__item-about">
+                                <a href="#" class="about__link">
+                                    <span class="js-translate">Register an account</span>
+                                </a>
+                                <span class="js-translate">&nbsp;and choose an investment portfolio</span>
+                            </div>
+                            <div class="l-steps__item-description">
+                                <span class="js-translate">On our website, you can find a convenient registration form consisting of several simple fields. Just in case, we use special technologies to correct any of your registration mistakes, even intentional.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 cut big_p">
+                        <div class="l-steps__item">
+                            <div class="l-steps__item-number">
+                                <span>2.</span>
+                            </div>
+                            <div class="l-steps__item-about">
+                                <span class="js-translate">Choose an investment portfolio that fits your needs</span>
+                            </div>
+                            <div class="l-steps__item-description">
+                                <span class="js-translate">Once in the back office, review the list of investment portfolios and choose one <br>to invest in.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 cut">
+                        <div class="l-steps__item">
+                            <div class="l-steps__item-number">
+                                <span>3.</span>
+                            </div>
+                            <div class="l-steps__item-about">
+                                <span class="js-translate">Fund the account in a convenient way and buy a portfolio</span>
+                            </div>
+                            <div class="l-steps__item-description">
+                                <span class="js-translate">You can easily make a deposit to your account via bank transfer or electronic payment systems.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -116,15 +209,15 @@
         </div>
         <div class="row">
             @foreach($data['news'] as $news)
-            <div class="col-xs-12 col-md-4">
-                <div class="news__item">
-                    <h4>{{ $news['title'] }}</h4>
-                    <div class="overflow news__overflow">
-                        <p>{{ $news['description'] }}</p>
+                <div class="col-xs-12 col-md-4">
+                    <div class="news__item">
+                        <h4>{{ $news['title'] }}</h4>
+                        <div class="overflow news__overflow">
+                            <p>{{ $news['description'] }}</p>
+                        </div>
+                        <a href="{{ route('news') }}" class="about__link">Читать далее...</a>
                     </div>
-                    <a href="{{ route('news') }}" class="about__link">Читать далее...</a>
                 </div>
-            </div>
             @endforeach
         </div>
     </div>
@@ -169,8 +262,8 @@
                         </ul>
                     </div>
                     <div class="col-xs-12 col-md-6">
-                        <h4>Соц. сети:</h4>
-                        @foreach($data['contacts']['social'] as $soc)
+                        <h4>Поделиться:</h4>
+                        @foreach($data['contacts']['social']['share'] as $soc)
                             <a href="{{ $soc['link'] }}">
                                 <img src="{{ asset($soc['img']."_black.svg") }}" alt="" class="contacts__img">
                             </a>
@@ -190,7 +283,8 @@
                     </div>
                     <div class="form-group @if( is_error('email') )has-error @endif">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="email@example.com">
+                        <input type="email" class="form-control" name="email" id="email"
+                               placeholder="email@example.com">
                         @if( is_error('email') )
                             <span class="help-block">{{ $errors->first('email') }}</span>
                         @endif
@@ -209,28 +303,28 @@
 
                 {{--{!! Form::open(['route' => 'create-feedback', 'class' => 'form']) !!}--}}
                 {{--<div class="form-group @if( is_error('name') )has-error @endif">--}}
-                    {{--{!! Form::label('name', 'Имя') !!}--}}
-                    {{--{!! Form::text('name', '', ['placeholder' => 'Ваше имя', 'id' => 'name', 'class' => 'form-control', 'maxlength' => "255", 'required' => 'required']) !!}--}}
-                    {{--@if( is_error('name') )--}}
-                        {{--<span class="help-block">{{ $errors->first('name') }}</span>--}}
-                    {{--@endif--}}
+                {{--{!! Form::label('name', 'Имя') !!}--}}
+                {{--{!! Form::text('name', '', ['placeholder' => 'Ваше имя', 'id' => 'name', 'class' => 'form-control', 'maxlength' => "255", 'required' => 'required']) !!}--}}
+                {{--@if( is_error('name') )--}}
+                {{--<span class="help-block">{{ $errors->first('name') }}</span>--}}
+                {{--@endif--}}
                 {{--</div>--}}
                 {{--<div class="form-group @if( is_error('email') )has-error @endif">--}}
-                    {{--{!! Form::label('email', 'Email') !!}--}}
-                    {{--{!! Form::email('email', '', ['placeholder' => 'email@example.com', 'id' => 'email', 'class' => 'form-control', 'maxlength' => "255", 'required' => 'required']) !!}--}}
-                    {{--@if( is_error('email') )--}}
-                        {{--<span class="help-block">{{ $errors->first('email') }}</span>--}}
-                    {{--@endif--}}
+                {{--{!! Form::label('email', 'Email') !!}--}}
+                {{--{!! Form::email('email', '', ['placeholder' => 'email@example.com', 'id' => 'email', 'class' => 'form-control', 'maxlength' => "255", 'required' => 'required']) !!}--}}
+                {{--@if( is_error('email') )--}}
+                {{--<span class="help-block">{{ $errors->first('email') }}</span>--}}
+                {{--@endif--}}
                 {{--</div>--}}
                 {{--<div class="form-group @if( is_error('question') )has-error @endif">--}}
-                    {{--{!! Form::label('question', 'Вопрос') !!}--}}
-                    {{--{!! Form::textarea('question', '', ['rows' => '2', 'id' => 'question', 'class' => 'form-control contacts__textarea', 'required' => 'required']) !!}--}}
-                    {{--@if( is_error('question') )--}}
-                        {{--<span class="help-block">{{ $errors->first('question') }}</span>--}}
-                    {{--@endif--}}
+                {{--{!! Form::label('question', 'Вопрос') !!}--}}
+                {{--{!! Form::textarea('question', '', ['rows' => '2', 'id' => 'question', 'class' => 'form-control contacts__textarea', 'required' => 'required']) !!}--}}
+                {{--@if( is_error('question') )--}}
+                {{--<span class="help-block">{{ $errors->first('question') }}</span>--}}
+                {{--@endif--}}
                 {{--</div>--}}
                 {{--<div class="form-group">--}}
-                    {{--{!! Form::button('Отправить', ['class' => 'btn btn-success']) !!}--}}
+                {{--{!! Form::button('Отправить', ['class' => 'btn btn-success']) !!}--}}
                 {{--</div>--}}
                 {{--{!! Form::close() !!}--}}
             </div>
@@ -250,39 +344,39 @@
 @include('partial.scripts')
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         $(".main-carousel").owlCarousel({
-            items : 1,
+            items: 1,
             autoplay: true,
             loop: true,
             dots: true,
         });
         $(".rate-carousel").owlCarousel({
-            items : 3,
+            items: 3,
             autoplay: true,
             loop: true,
             dots: true,
-            margin:10,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
                     stagePadding: 50
                 },
-                600:{
-                    items:3
+                600: {
+                    items: 3
                 },
-                1000:{
-                    items:3
+                1000: {
+                    items: 3
                 }
             }
         });
-        /*$("#menu").on("click","a", function (event) {
-            event.preventDefault();
-            var id  = $(this).attr('href'),
-                top = $(id).offset().top - 50;
-            $('body,html').animate({scrollTop: top}, 500);
-        });*/
+        $(".anchor_a").on("click", function (event) {
+         event.preventDefault();
+         var id  = $(this).attr('href'),
+         top = $(id).offset().top - 50;
+         $('body,html').animate({scrollTop: top}, 500);
+         });
     });
 </script>
 </body>
--- resources/views/partial/mainmenu.blade.php
+++ resources/views/partial/mainmenu.blade.php
@@ -10,21 +10,29 @@
     <div id="navbar" class="navbar-collapse collapse main-menu__wrap">
         <ul class="nav navbar-nav main-menu__menu_center" id="menu">
             <li><a href="{{ route('index') }}">Главная</a></li>
-            <li><a href="{{ route('about') }}">О проекте</a></li>
+            <li class="dropdown">
+                <a href="{{ route('about') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">О проекте <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                </ul>
+            </li>
+            <li><a href="{{ route('about') }}">Акции</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Партнерам <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Пополнить счет</a></li>
+                    <li><a href="#">Вывести средства</a></li>
+                    <li><a href="#">Тарифы</a></li>
+                </ul>
+            </li>
             <li><a href="{{ route('news') }}">Новости</a></li>
             <li><a href="{{ route('questions') }}">Вопрос-ответ</a></li>
             <li>
                 <a href="{{ url('/login') }}" class="main-menu__register-link">Войти/</a>
                 <a href="{{ url('/register') }}" class="main-menu__register-link">регистрация</a>
             </li>
-            <li><a href="{{ route('regulations') }}">Правила</a></li>
-            <li><a href="{{ route('contacts') }}">Контакты</a></li>
-            <li>
-                @foreach($data['contacts']['social'] as $soc)
-                    <a href="{{ $soc['link'] }}" class="main-menu__social-link"><img src="{{ asset($soc['img'].".svg") }}" alt="" class="main-menu__social-link__img"></a>
-                @endforeach
-            </li>
-
         </ul>
     </div><!--/.nav-collapse -->
 </div>
