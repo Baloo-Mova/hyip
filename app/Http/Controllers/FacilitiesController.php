dbe050d032461412e434e0700f4208d4c44cc0ac
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-09-01 16:34:21 +0300

    update info for pages

--- app/Http/Controllers/FacilitiesController.php
+++ app/Http/Controllers/FacilitiesController.php
@@ -14,11 +14,33 @@ class FacilitiesController extends Controller
         $data = [
             'contacts' =>[
                 'social' => [
-                    'vk' => [
-                        'img' => 'img/vk', 'link' => 'http://google.com.ua'
+                    'links' => [
+                        'vk' => [
+                            'img' => 'img/vk', 'link' => 'http://google.com.ua'
+                        ],
+                        'instagram' => [
+                            'img' => 'img/instagram', 'link' => 'http://google.com.ua'
+                        ]
                     ],
-                    'instagram' => [
-                        'img' => 'img/instagram', 'link' => 'http://google.com.ua'
+                    'share' => [
+                        'vk' => [
+                            'img' => 'img/vk', 'link' => 'http://google.com.ua'
+                        ],
+                        'fb' => [
+                            'img' => 'img/fb', 'link' => 'http://google.com.ua'
+                        ],
+                        'ok' => [
+                            'img' => 'img/ok', 'link' => 'http://google.com.ua'
+                        ],
+                        'tw' => [
+                            'img' => 'img/tw', 'link' => 'http://google.com.ua'
+                        ],
+                        'tl' => [
+                            'img' => 'img/tl', 'link' => 'http://google.com.ua'
+                        ],
+                        'instagram' => [
+                            'img' => 'img/instagram', 'link' => 'http://google.com.ua'
+                        ]
                     ]
                 ]
             ]
@@ -32,11 +54,33 @@ class FacilitiesController extends Controller
         $data = [
             'contacts' =>[
                 'social' => [
-                    'vk' => [
-                        'img' => 'img/vk', 'link' => 'http://google.com.ua'
+                    'links' => [
+                        'vk' => [
+                            'img' => 'img/vk', 'link' => 'http://google.com.ua'
+                        ],
+                        'instagram' => [
+                            'img' => 'img/instagram', 'link' => 'http://google.com.ua'
+                        ]
                     ],
-                    'instagram' => [
-                        'img' => 'img/instagram', 'link' => 'http://google.com.ua'
+                    'share' => [
+                        'vk' => [
+                            'img' => 'img/vk', 'link' => 'http://google.com.ua'
+                        ],
+                        'fb' => [
+                            'img' => 'img/fb', 'link' => 'http://google.com.ua'
+                        ],
+                        'ok' => [
+                            'img' => 'img/ok', 'link' => 'http://google.com.ua'
+                        ],
+                        'tw' => [
+                            'img' => 'img/tw', 'link' => 'http://google.com.ua'
+                        ],
+                        'tl' => [
+                            'img' => 'img/tl', 'link' => 'http://google.com.ua'
+                        ],
+                        'instagram' => [
+                            'img' => 'img/instagram', 'link' => 'http://google.com.ua'
+                        ]
                     ]
                 ]
             ]
