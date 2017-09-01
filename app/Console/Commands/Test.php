16ebdb2e1162ab0517b26b7d5895a711d97e3ba9
solomon <sergious-91@mail.ru>
2017-09-01 15:56:34 +0300

    йцу

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $gen = Factory::create();
+        dd($gen->uuid);
+    }
+}
