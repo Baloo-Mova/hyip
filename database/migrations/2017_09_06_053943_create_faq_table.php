963c70a7eb0474833c7236e1c1896a48b5fbb562
Admin <ntfake@yandex.ru>
2017-09-06 20:11:30 +0300

    faq: migration

---
+++ database/migrations/2017_09_06_053943_create_faq_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFaqTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faq', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->string('answer');
+            $table->boolean('is_active')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faq');
+    }
+}
