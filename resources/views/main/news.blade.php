7d15238c22e64ad17919706d99154d1377496c39
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-09-13 14:10:33 +0300

    update news list style

--- resources/views/main/news.blade.php
+++ resources/views/main/news.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <div class="col-xs-12">
                 <h1>Новости</h1>
+                <hr>
             </div>
         </div>
         
@@ -18,14 +19,14 @@
                                     <img src="{{ asset('media/uploads/blog').'/'.$news['preview'] }}" alt="">
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-md-9">
+                            <div class="col-xs-12 col-md-9 left-text">
                                 <div class="news__description">
                                     <h4>{{ $news['title'] }}</h4>
                                     <p>
                                         {!!  $news['content'] !!}
-                                        <a href="{{ route('news.show', ['id' => $news['id']]) }}" class="red-text">подробнее</a>
                                     </p>
                                 </div>
+                                <a href="{{ route('news.show', ['id' => $news['id']]) }}" class="red-text">подробнее</a>
                             </div>
                         </div>
 
