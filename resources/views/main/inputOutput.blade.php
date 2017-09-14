11dd2101bdfa3c594ae2983f4255ab75085a0bff
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-09-14 20:43:48 +0300

    update input output

--- resources/views/main/inputOutput.blade.php
+++ resources/views/main/inputOutput.blade.php
@@ -2,26 +2,15 @@
 
 @section('content')
     <div class="container">
-        <h1 class="page-header">Ввод/вывод </h1>
-        <div id="exTab2" class="">
-            <ul class="nav nav-tabs">
-                <li class="{{ $type == "input" ? "active" : "" }}">
-                    <a href="#in" data-toggle="tab">Пополнить счет</a>
-                </li>
-                <li class="{{ $type == "output" ? "active" : "" }}">
-                    <a href="#out" data-toggle="tab">Вывести средства</a>
-                </li>
-            </ul>
-
-            <div class="tab-content ">
-                <div class="tab-pane {{ $type == "input" ? "active" : "" }}" id="in">
-                    <h2>{{ $data['input']['title'] }}</h2>
-                    {!!  $data['input']['description']  !!}
-                </div>
-                <div class="tab-pane {{ $type == "output" ? "active" : "" }}" id="out">
-                    <h2>{{ $data['output']['title'] }}</h2>
-                    {!! $data['output']['description']  !!}
-                </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <h1 class="page-header">Ввод/вывод </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>{{ $data[$type]['title'] }}</h2>
+                {!!  $data[$type]['description']  !!}
             </div>
         </div>
     </div>
--- resources/views/partial/usersidemenu.blade.php
+++ resources/views/partial/usersidemenu.blade.php
@@ -5,7 +5,7 @@
     <li class="{{ Request::path() == 'cabinet/profile' ? 'active' : ''}}"><a href="{{ route('profile') }}">Профиль</a></li>
     <li class="{{ \Request::is('cabinet/tariff/*') ? 'active' : ''}}"><a href="{{ route('tariff', ['id' => -1]) }}">Тариф</a></li>
     <li class="{{ Request::path() == 'cabinet/dialogs' ? 'active' : ''}}"><a href="{{ route('dialogs') }}">Сообщения</a></li>
-    <li class="{{ Request::path() == 'cabinet/facilities/operations' ? 'active' : ''}}"><a href="{{ route('facilities.operations') }}">Операции</a></li>
+    {{--<li class="{{ Request::path() == 'cabinet/facilities/operations' ? 'active' : ''}}"><a href="{{ route('facilities.operations') }}">Операции</a></li>--}}
     <li class="{{ Request::path() == 'cabinet/support' ? 'active' : ''}}"><a href="{{ route('support') }}">Поддержка</a></li>
 </ul>
 
