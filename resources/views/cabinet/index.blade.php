2d70cdac2289af8d02964345966438791494cc0a
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-09-15 18:21:18 +0300

    update user cabinet page

--- resources/views/cabinet/index.blade.php
+++ resources/views/cabinet/index.blade.php
@@ -5,18 +5,33 @@
     <h1 class="page-header">Личный кабинет пользователя</h1>
     <div class="row">
         <div class="col-xs-12">
-            <ul class="user-cabinet__menu">
-                <li>Баланс: {{ $user->balance }}₽</li>
-                @if(isset($user->subscribe_id))
-                    <li>Ваш тариф: {{$user->subscription->name}}</li>
-                    <li title="Срок действия тарифа">
-                        Тариф истечет через: {{ $payedForDiff->days. '  '.$payedForDiff->h.':'.$payedForDiff->i }}
-                    </li>
-                @else
-                    <li>Тариф отсутствует</li>
-                @endif
-                <li>Ваша реферальная ссылка: <b>{{route('ref.add',['id'=>$user->ref_link])}}</b></li>
-            </ul>
+            <div class="news__item row user__item">
+                <div class="col-xs-12 text-center">
+                    <div>
+                        <img src="{{ asset('img/avatar.png') }}" alt="" class="user_avatar">
+                        <h4>{{ $user->login }}</h4>
+                        <h5>{{route('ref.add',['id'=>$user->ref_link])}}</h5>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="user-info__item">
+                        <a href="{{ isset($user->subscribe_id) ? route('tariff', ['id' => $user->subscribe_id]) : "#" }}" class="tariff___term">{{ isset($user->subscribe_id) ? $user->subscription->name : "-" }}</a>
+                        <h4>Тариф</h4>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="user-info__item">
+                        <p class="tariff___term">{{ $user->balance }}₽</p>
+                        <h4>Баланс</h4>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="user-info__item">
+                        <p class="tariff___term">{{ isset($user->subscribe_id) ? $payedForDiff->days. '  '.$payedForDiff->h.':'.$payedForDiff->i : "-"}}</p>
+                        <h4>Тариф истечет через:</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
