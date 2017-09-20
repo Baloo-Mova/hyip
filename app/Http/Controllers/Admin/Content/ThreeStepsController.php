4e42b4d8c0e10e8dc1ea424e9838be1acbb9b69d
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-09-20 09:51:12 +0300

    fix 3 steps controller

--- app/Http/Controllers/Admin/Content/ThreeStepsController.php
+++ app/Http/Controllers/Admin/Content/ThreeStepsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin\Content;
 
-use App\Models\ThreeSteps;
+use App\Models\MainPage\ThreeSteps;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
