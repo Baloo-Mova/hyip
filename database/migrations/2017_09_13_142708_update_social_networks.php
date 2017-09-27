45d0c4c161b53669fecedab525a72a9068f26b94
solomon <sergious-91@mail.ru>
2017-09-27 22:22:16 +0300

    qwe

--- database/migrations/2017_09_13_142708_update_social_networks.php
+++ database/migrations/2017_09_13_142708_update_social_networks.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateSocialNetworksNew extends Migration
+class UpdateSocialNetworks extends Migration
 {
     /**
      * Run the migrations.
