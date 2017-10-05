1d3b4977e944ebe0e8644cdbf7c48bea78d9b5e3
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-10-05 20:37:33 +0300

    update send message to all users

--- app/Http/Controllers/Admin/SendingMessagesController.php
+++ app/Http/Controllers/Admin/SendingMessagesController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\SendingMessages\SendRequest;
 use App\Jobs\SendingMessagesToEmail;
+use App\Jobs\SendMessagesToAll;
 use App\Models\Message;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class SendingMessagesController extends BaseController
 {
@@ -37,28 +39,9 @@ class SendingMessagesController extends BaseController
         $user_id = \Auth::guard('admin')->id();
         $message = $request->get('text');
 
-        switch ($request->get('type')) {
-            case 1:
-                $user_ids = User::where('id', '!=', $user_id)->get()->pluck('id');
-                $res = [];
-                foreach ($user_ids as $id) {
-                    $res[] = [
-                        'from_user' => $user_id,
-                        'to_user'   => $id,
-                        'message'   => $message,
-                        'created_at'=> Carbon::now(),
-                        'updated_at'=> Carbon::now(),
-                    ];
-                }
-                Message::insert($res);
-                break;
-            case 2:
-                $emails = User::where('id', '!=', $user_id)->take(2)->get()->pluck('email');
-                $job = (new SendingMessagesToEmail($emails, $message))->onQueue('emails');
-                dispatch($job);
-                break;
-        }
+        $this->dispatch(new SendMessagesToAll($request->get('type'), $message, $user_id));
 
+        Session::flash('messages', ['Рассылка сообщений успешно начата!']);
         return redirect()->back();
     }
 }
---
+++ app/Jobs/SendMessagesToAll.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Settings;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use PHPMailer\PHPMailer\PHPMailer;
+use App\Models\Chats;
+use App\Models\Message;
+
+class SendMessagesToAll implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $type;
+    protected $message;
+    protected $fromId;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($type, $message, $from_id)
+    {
+        $this->type = $type;
+        $this->message = $message;
+        $this->fromId = $from_id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if($this->type == 2){
+            $mail = new PHPMailer;
+            $settings = Settings::find(1);
+            if(!isset($settings)){
+                return false;
+            }
+        }
+
+        $offset = 0;
+        $limit = 100;
+
+        while(true){
+            $users = User::offset($offset)
+                ->limit($limit)
+                ->get();
+            if(count($users) == 0){
+                break;
+            }
+
+            $messages = [];
+
+            foreach ($users as $user){
+
+                if($user->id == $this->fromId){
+                    continue;
+                }
+                if($this->type == 1){
+
+                    $chat = Chats::where([
+                        ['creator_id', '=', $this->fromId],
+                        ['to_id', '=', $user->id]
+                    ])
+                    ->orWhere([
+                        ['creator_id', '=', $user->id],
+                        ['to_id', '=', $this->fromId]
+                    ])->first();
+
+
+                    if(!isset($chat)){
+                        $chat = new Chats();
+                        $chat->creator_id = $this->fromId;
+                        $chat->to_id = $user->id;
+                        $chat->save();
+                    }
+                    $messages[] = [
+                        'from_user' => $chat->creator_id,
+                        'to_user' => $chat->to_id,
+                        'message' => $this->message,
+                        'chat_id' => $chat->id
+                    ];
+
+
+                }else{
+
+                    try {
+                        $mail->isSMTP();
+                        $mail->Host = $settings->smtp;
+                        $mail->SMTPAuth = true;
+                        $mail->Username = $settings->smtp_login;
+                        $mail->Password = $settings->smtp_pasw;
+                        $mail->SMTPSecure = $settings->smtp_secure;
+                        $mail->Port = $settings->smtp_port;
+                        $mail->CharSet = 'UTF-8';
+                        $mail->setFrom($settings->smtp_login);
+                        $mail->addAddress($user->email);
+
+                        $mail->Subject = "Сообщение от админа ".env("APP_NAME");
+                        $mail->Body = $this->message;
+                        if(preg_match("/<[^<]+>/", $this->message, $m) != 0){
+                            $mail->IsHTML(true);
+                        }
+
+                        $mail->send();
+                    } catch (\Exception $ex) {
+                        continue;
+                    }
+                    $mail->ClearAllRecipients();
+                    sleep(2);
+                }
+
+            }
+
+            if($this->type == 1){
+                Message::Insert($messages);
+            }else{
+
+            }
+
+            $offset += $limit;
+
+        }
+
+        return true;
+
+    }
+}
