53878d5808a87ae8b8664cede0f3797a8596c6bd
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-10-26 12:43:16 +0300

    update footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 text-center">
-                <a href="{{ route('privacy.policy') }}" class="white-text">@lang('messages.privacy_policy')</a>
+                <a href="{{ route('regulations') }}" class="white-text">@lang('messages.privacy_policy')</a>
                 <p>@lang('messages.copiright'). 2017</p>
             </div>
         </div>
