cddf41d1693e1c4976eefe92b3b3676200a2fedd
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-11-13 13:10:40 +0200

    fix colors

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends BaseVerifier
      */
     protected $except = [
         '/facilities/status',
-        '/questions-list'
+        '/questions-list',
+        '/news-list'
     ];
 }
