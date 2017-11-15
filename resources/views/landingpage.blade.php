e6139127ea7c5d3c872abe720a7016484147565d
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-11-15 15:16:13 +0200

    add icon

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="WhiteCoin">
+    <link rel="icon" sizes="192x192" href="{{ asset('img/logo_md.png') }}">
     <title>{{env('APP_NAME')}}</title>
     @include('partial.head')
     @yield('css')
