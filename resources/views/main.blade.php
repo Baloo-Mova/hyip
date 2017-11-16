6898fc169af766426a8e796ace0c8e5e92310edd
DESKTOP-RJ6FB6Q\Sergey <petliak.serhii@gmail.com>
2017-11-16 10:51:32 +0200

    add meta-tags

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,14 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="WhiteCoin">
-    <link rel="icon" sizes="192x192" href="{{ asset('img/logo_md.png') }}">
-    <meta content="{{ asset('img/logo_md.png') }}" property="og:image" />
-    <meta content="{{env('APP_NAME')}}" property="og:site_name" />
-    <meta content="object" property="og:type" />
-    <meta content="WhiteCoin" property="og:title" />
-    <meta content="Описание" property="og:description" />
     <title>{{env('APP_NAME')}}</title>
+    @include("partial.meta_tags")
     @include('partial.head')
     @yield('css')
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
---
+++ resources/views/partial/meta_tags.blade.php
@@ -0,0 +1,16 @@
+<meta property="og:type" content="article"/>
+<meta property="og:title" content="{{env('APP_NAME')}}"/>
+<meta property="og:description" content="{{ env("META_DESCRIPTION") }}"/>
+<meta property="og:image" content="{{ asset('img/meta_logo.png') }}"/>
+<meta property="og:site_name" content="{{env('APP_NAME')}}"/>
+<link rel="icon" sizes="64x64" href="{{ asset('img/meta_icon.jpg') }}">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="{{env('META_AUTHOR')}}">
+<meta name="twitter:title" content="{{env('APP_NAME')}}">
+<meta name="twitter:description" content="{{ env("META_DESCRIPTION") }}">
+<meta name="twitter:image" content="{{ asset('img/meta_logo.png') }}">
+
+<meta itemprop="name" content="{{env('APP_NAME')}}"/>
+<meta itemprop="description" content="{{ env("META_DESCRIPTION") }}"/>
+<meta itemprop="image" content="{{ asset('img/meta_logo.png') }}"/>
